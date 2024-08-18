fc34db7ee5257b1bf41c4476c41eaf89f56a1727
Steddy <qwesteddyoff@gmail.com>
2024-08-18 19:30:55 +0300

    add DeviceStore

---
+++ src/store/DeviceStore.js
@@ -0,0 +1,59 @@
+import { makeAutoObservable } from "mobx";
+
+export default class DeviceStore {
+  constructor() {
+    this._types = [
+      { id: 1, name: "TVs" },
+      { id: 2, name: "smartphones" },
+    ];
+    this._brands = [
+      { id: 1, brand: "Samsung" },
+      { id: 2, brand: "Apple" },
+    ];
+    this._devices = [
+      {
+        id: 1,
+        name: "Iphone 11",
+        price: 15000,
+        rating: 4,
+        img: `https://www.apple.com/newsroom/images/tile-images/Apple_iphone_11-rosette-family-lineup-091019.jpg.news_app_ed.jpg`,
+      },
+      {
+        id: 2,
+        name: "Iphone 11 Pro",
+        price: 25000,
+        rating: 5,
+        img: `https://www.apple.com/newsroom/images/tile-images/Apple_iphone_11-rosette-family-lineup-091019.jpg.news_app_ed.jpg`,
+      },
+      {
+        id: 3,
+        name: "Iphone 11 Pro Max",
+        price: 50000,
+        rating: 5,
+        img: `https://www.apple.com/newsroom/images/tile-images/Apple_iphone_11-rosette-family-lineup-091019.jpg.news_app_ed.jpg`,
+      },
+    ];
+
+    makeAutoObservable(this);
+  }
+
+  getTypes(types) {
+    this._types = types;
+  }
+  getBrands(brands) {
+    this._brands = brands;
+  }
+
+  getDevices(devices) {
+    this._devices = devices;
+  }
+  get types() {
+    return this._types;
+  }
+  get brands() {
+    return this._brands;
+  }
+  get devices() {
+    return this._devices;
+  }
+}
