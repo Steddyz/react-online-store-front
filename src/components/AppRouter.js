c52d4f4cfee3870f33878087af51117534800388
Steddy <qwesteddyoff@gmail.com>
2024-08-18 11:51:42 +0300

    first commit + add router

---
+++ src/components/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+import Shop from "../pages/Shop";
+export default function AppRouter() {
+  let isAuth = false;
+  return (
+    <Routes>
+      {isAuth &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={Component} exact />
+        ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={Component} exact />
+      ))}
+      <Route path="*" element={<Shop replace to="/" />} />
+    </Routes>
+  );
+}
---
+++ src/utils/const.js
@@ -0,0 +1,6 @@
+export const ADMIN_ROUTE = "/admin";
+export const SHOP_ROUTE = "/";
+export const BASKET_ROUTE = "/basket";
+export const REGISTER_ROUTE = "/register";
+export const LOGIN_ROUTE = "/login";
+export const DEVISE_ROUTE = "/devise";
